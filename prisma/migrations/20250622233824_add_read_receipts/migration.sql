247895f59fdbceb502234a10233600b21932eaf5
Aden Lindsay <lindsaya542@gmail.com>
2025-06-23 09:25:11 +0930

    fix: remove leftover mirgation

--- prisma/migrations/20250622233824_add_read_receipts/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `read` on the `Message` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Message` DROP COLUMN `read`,
-    ADD COLUMN `readAt` DATETIME(3) NULL;
