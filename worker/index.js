1f9071e926abf20b48e02a43d12c8af8339cc089
Aden Lindsay <140392385+AdenMGB@users.noreply.github.com>
2026-02-03 20:06:48 +0800

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -10,7 +10,7 @@ export default {
     const corsHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-User-ID',
     };
 
     if (request.method === "OPTIONS") {
